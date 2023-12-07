c3577762b5fd870d1b5c28c6967b293b650e059f
nirgluzman <nir.gluzman@gmail.com>
2023-12-07 22:27:48 +0100

    add NewPost component and its CSS module stylesheet

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,17 +1,22 @@
 import Post from './Post';
+import NewPost from './NewPost';
+
 import classes from './PostsList.module.css';
 
 export default function PostsList() {
 	return (
-		<ul className={classes.posts}>
-			<Post
-				author='Max'
-				body='bla bla'
-			/>
-			<Post
-				author='Idan'
-				body='bla bla'
-			/>
-		</ul>
+		<>
+			<NewPost />
+			<ul className={classes.posts}>
+				<Post
+					author='Max'
+					body='bla bla'
+				/>
+				<Post
+					author='Idan'
+					body='bla bla'
+				/>
+			</ul>
+		</>
 	);
 }
